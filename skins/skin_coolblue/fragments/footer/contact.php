b8fc6a8cf2d67726a466aae0c148078be77b1ffd
Scott Lewis <me@scottlewis.info>
2012-03-28 23:57:07 -0400

    Deleted skins folder and contents. Skins have been moved to individual projects.

--- skins/skin_coolblue/fragments/footer/contact.php
+++
@@ -1,9 +0,0 @@
-<?php defined('SKYBLUE') or die("Bad file request"); ?>
-<h3>Contact Info</h3>
-<p>
-	<strong>Phone: </strong>[[site.contact_phone]]<br/>
-	<strong>Fax: </strong>[[site.contact_fax]]
-</p>
-<p><strong>Address: </strong>[[site.contact_address]]</p>
-<p><strong>E-mail: </strong>[[site.contact_email]]</p>
-<p>[[page.link(4)]]</p>
