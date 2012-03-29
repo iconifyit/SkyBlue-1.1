eb0274995985116c0479baf4bbfa709029e93843
Scott Lewis <me@scottlewis.info>
2012-03-28 23:54:48 -0400

    Deleted skins folder and contents. Skins have been moved to individual projects.

--- skins/skin_coolblue/fragments/footer/scripts.php
+++
@@ -1,4 +0,0 @@
-<?php defined('SKYBLUE') or die("Bad file request"); ?>
-<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]--> 
-<script src="[[site.url]][[skin.path]]js/jflickrfeed/jflickrfeed.min.js"></script>
-<script type="text/javascript" src="[[site.url]][[skin.path]]js/lightbox/js/jquery.lightbox-0.5.js"></script>
