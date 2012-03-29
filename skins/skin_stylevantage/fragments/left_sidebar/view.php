c6bcab0a97523822bd7a0711a3a3ecd6a887ea5e
Scott Lewis <me@scottlewis.info>
2012-03-29 00:48:28 -0400

    Trying to delete skins

--- skins/skin_stylevantage/fragments/left_sidebar/view.php
+++
@@ -1,4 +0,0 @@
-<!-- Add your code here -->
-<p>Left Side Bar</p>
-
-<!--#plugin:fragment(links)-->
--- skins/skin_techjunkie/fragments/footer/view.php
+++
@@ -1,5 +0,0 @@
-<div id="footer-content">
-	<div id="footer-bottom">                
-		<p>Powered by <a href="http://www.skybluecanvas.com">SkyBlueCanvas</a></p>                    
-	</div>    
-</div>
